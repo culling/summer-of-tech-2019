dcde5cf8634fdab4f3970373a0d2f491ab2fa0d6
Gene Culling <culling66@gmail.com>
2019-08-24 11:03:49 +1200

    created server.py to work with heroku app

--- server.py
+++ server.py
@@ -1,2 +1,19 @@
-PORT = environ.get('PORT', 8000);
-http.server PORT
+import http.server
+import socketserver
+
+try:
+    environ
+except: 
+    environ = None
+
+if environ is None :
+    PORT = 8000
+else:
+    PORT = environ.get('PORT')
+
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
