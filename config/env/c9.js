ec5f3dfa25703366782d19513922c5a602b3228c
Gene Culling <culling66@gmail.com>
2019-08-24 11:42:50 +1200

    added deployment options

--- config/env/c9.js
+++ config/env/c9.js
@@ -1,11 +1,10 @@
-//DEV on Windows with Heroku BE
 module.exports = {
-    port:   5000,
+    port: 8080,
     mongoPort:  27017,
     mongoDatabase:      "mydb",
     mongoServer:        "localhost",
     pageTitle:          "Free Code Camp - Votes",
-    mongoUrl:            process.env.MONGODB_URI || `mongodb://localhost:27017/mydb` ,
+    mongoUrl:           process.env.MONGODB_URI || `mongodb://localhost:27017/mydb`,
     publicHostname:     "localhost",
-    publicUrl:          "localhost:5000"
-};
+    publicUrl:          "localhost:8080"
+}
---
+++ config/env/heroku.js
@@ -0,0 +1,8 @@
+module.exports={
+    port:               process.env.PORT || 80,
+    mongoPort:          37882,
+    pageTitle:          "Free Code Camp - Votes",
+    mongoUrl:           process.env.MONGODB_URI,
+    publicHostname:     "culling-fcc-pintrest.herokuapp.com",
+    publicUrl:          "culling-fcc-pintrest.herokuapp.com"
+}
