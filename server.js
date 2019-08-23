15a26d5df62212b854e26a8897f4c9a23eaab785
Gene Culling <culling66@gmail.com>
2019-08-24 11:39:06 +1200

    copied in from previous project

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express     = require('./config/express');
@@ -9,3 +10,16 @@ var port = 80;
 app.listen(port);
 module.exports = app;
 console.log('server running at http://localhost:' + port);
+*/
+
+console.log("server.js loaded");
+process.env.NODE_ENV = process.env.NODE_ENV  || "development" ;
+var config  = require("./config/config.js");
+
+var express = require("./config/express");
+var app     = express();
+
+app.listen(config.port, function(){
+    console.log(process.env.NODE_ENV);
+    console.log("Listening on Port: "+ config.port);
+});
